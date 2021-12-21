abd5599843779705c8b417a0bbca196a37acb20c
ManuelaVignal <manuela.vignal@libero.it>
2021-12-21 13:05:28 +0100

    messaggio1

--- hello/main/App.java
+++ hello/main/App.java
@@ -4,6 +4,7 @@ public class App {
 
           public static void main(String[] args){
                  System.out.println("saluti da java....");
+                 System.out.println("ciao");
                  
                  
                  
