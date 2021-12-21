deba4c8d51f773bd6a9e36920b426986562bb554
ManuelaVignal <manuela.vignal@libero.it>
2021-12-21 11:44:50 +0100

    start

---
+++ hello/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+public class App {
+
+          public static void main(String[] args){
+                 System.out.println("saluti da java....");
+                 
+
+ 
+          }
+
+
+
+}
+
+
