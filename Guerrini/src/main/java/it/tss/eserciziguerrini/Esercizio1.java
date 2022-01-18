2ca49943f313e01aceef12053400ba7b179586bd
ManuelaVignal <manuela.vignal@libero.it>
2022-01-18 12:42:12 +0100

    aggiornamento guerrini

--- Guerrini/src/main/java/it/tss/eserciziguerrini/Esercizio1.java
+++ Guerrini/src/main/java/it/tss/eserciziguerrini/Esercizio1.java
@@ -4,6 +4,8 @@
  */
 package it.tss.eserciziguerrini;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tss
@@ -12,10 +14,40 @@ public class Esercizio1 {
 
     public static void main(String[] args) {
 
-        int myvoto = 9;
+        int myvoto;
         // per essere ammesso almeno 5
+        
+        String vototx = JOptionPane.showInputDialog("inserisci la media dei voti");
+        // converto in intero la stringa vototx
+        boolean lode = 
+                
+                 vototx.equalsIgnoreCase("10 e lode") //lode diventa true se è vero
+                || 
+                   vototx.equalsIgnoreCase("10 con lode")
+                || 
+                   vototx.equalsIgnoreCase("10 lode")
+        
+        ;
+        
+        if(!lode)
+            myvoto = Integer.parseInt(vototx);//analizzattore sintattico parseInt, converte la frase in intero
+        else
+            myvoto=10;
+        
+        boolean  outofrange= (myvoto< 0 || myvoto>10);//outofrange è o true o false
+        
+        if (outofrange){ // if (!(myvoto >= 0 && myvoto <= 10));
+            
+        
+             System.out.println("NUMERO NON COMPRESO NEL RANGE");
+             return;
+             
+        }
+        
+        
+        else {
 
-        if (myvoto >= 5) {
+           if (myvoto >= 5 && !outofrange) {
 
             System.out.println("AMMESSO");
 
@@ -27,15 +59,21 @@ public class Esercizio1 {
                 if (myvoto >= 7 && myvoto <= 8) {
                     System.out.println("DISCRETO!!");
                 } else {
+                    
+                
                     System.out.println("OTTIMO!!!");
+                    if(lode) System.out.println("CON LODE");
                 }
             }
+            
+           
+            
 
-        } else {
+        }else{
 
             System.out.println("NON AMMESSO");
         }
 
+    
     }
 
-}
