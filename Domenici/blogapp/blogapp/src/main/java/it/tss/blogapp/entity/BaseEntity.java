3182e253022d775c68476329ac6e1eb3fda23232
ManuelaVignal <manuela.vignal@libero.it>
2022-03-22 14:30:20 +0100

    databaseGuerrini

--- Domenici/blogapp/blogapp/src/main/java/it/tss/blogapp/entity/BaseEntity.java
+++ Domenici/blogapp/blogapp/src/main/java/it/tss/blogapp/entity/BaseEntity.java
@@ -24,6 +24,7 @@ public abstract class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    
     @Version
     protected Long version;
     
