484cc0bd73442d8ba368755d1083355e8306c469
ManuelaVignal <manuela.vignal@libero.it>
2022-04-01 16:18:46 +0200

    01/04/2022

---
+++ blogapp-jakarta/blogapp/blogapp/src/main/java/it/tss/blogapp/boundary/UsersResource.java
@@ -0,0 +1,182 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.tss.blogapp.boundary;
+
+import it.tss.blogapp.JAXRSConfiguration;
+import it.tss.blogapp.JWTManager;
+import it.tss.blogapp.control.PostStore;
+import it.tss.blogapp.control.UserStore;
+import it.tss.blogapp.entity.Post;
+import it.tss.blogapp.entity.User;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.stream.JsonCollectors;
+import javax.validation.Valid;
+import javax.validation.constraints.AssertFalse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.ResourceContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+/**
+ *
+ * @author tss
+ */
+@Path("/users")
+@Tag(name = "Gestione Users", description = "Permette di gestire gli utenti di blogapp")
+public class UsersResource {
+
+    @Inject
+    private UserStore store;
+
+    @Inject
+    PostStore postStore;
+
+    @Context
+    ResourceContext rc;
+
+    @Context
+    UriInfo uriInfo;
+    
+     @Inject
+    JWTManager jwtManager;
+
+    @POST
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject login(@Valid Credential credential) {
+        User u =store.login(credential).orElseThrow(()-> new NotAuthorizedException(Response.status(Response.Status.UNAUTHORIZED).build()));
+        String jwt=jwtManager.generate(u);
+        return Json.createObjectBuilder().add("jwt", jwt)
+                .build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce l'elenco di tutti gli utenti")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo")
+    })
+    public List<User> all(@DefaultValue("1") @QueryParam("page") int page, @DefaultValue("10") @QueryParam("size") int size) {
+        return store.allPaginated(page, size);
+    }
+
+    @GET
+    @Path("allslice")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce l'elenco con informazioni ridotte di tutti gli utenti")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo")
+    })
+    public JsonArray allSlice() {
+        return store.all().stream().map(User::toJsonSlice).collect(JsonCollectors.toJsonArray());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Permette la registrazione di un nuovo utente")
+    @APIResponses({
+        @APIResponse(responseCode = "201", description = "Nuovo utente creato con successo")
+    })
+
+    public Response create(@Valid User entity) {
+        User saved = store.create(entity);
+        return Response.status(Response.Status.CREATED)
+                .entity(saved)
+                .build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Aggiorna i dati dell'utente")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Nuovo utente creato con successo")
+    })
+    
+    
+    public User update(@PathParam("id") Long id, @Valid User entity) {
+        User found = store.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+        entity.setId(id);
+
+        return store.save(entity);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce la risorsa utente identificata dall'ID")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Utente ritornato con successo"),
+        @APIResponse(responseCode = "404", description = "Utente non trovato")
+    })
+    public User find(@PathParam("id") Long id) {
+        return store.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Operation(description = "Elimina una risorsa Utente tramite l'ID")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Utente eliminato con successo"),
+        @APIResponse(responseCode = "404", description = "Utente non trovato")
+
+    })
+    public void delete(@PathParam("id") Long id) {
+        User found = store.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+        store.delete(found.getId());
+    }
+
+    @GET
+    @Path("{id}/posts")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce l'elenco di tutti i post dell'Utente")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo")
+    })
+    public List<Post> posts(@PathParam("id") Long id) {
+        return postStore.byUser(id);
+    }
+
+    @POST
+    @Path("{id}/posts")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Crea un nuovo Post per l'Utente")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo")
+    })
+    public Response createPost(@PathParam("id") Long id, @Valid Post entity) {
+        Post saved = postStore.save(entity);
+        return Response.status(Response.Status.CREATED)
+                .entity(saved)
+                .build();
+    }
+}
